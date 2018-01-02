7625f9d1f210917a8fb9c36e062f9c268de658cb
Jayanth L <lkench5@gmail.com>
2018-01-02 15:07:01 +0530

    include all files in a folder and put it in a HashMap

--- app/src/main/java/quantum/jayanthlokesh/bytearray2/MainActivity.java
+++ app/src/main/java/quantum/jayanthlokesh/bytearray2/MainActivity.java
@@ -15,14 +15,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     File file;
     private File[] listfiles;
     private String[] filepaths;
     private String[] filenames;
-    int i;
-    int j;
+    int i,j,x,y;
     int count=0;
     String byteArray = "";
     String encodedbase64value = "";
@@ -44,56 +44,64 @@ public class MainActivity extends AppCompatActivity {
             //Toast.makeText(this, "Hello " + listfiles.length, Toast.LENGTH_SHORT).show();
             filepaths = new String[listfiles.length];
             filenames = new String[listfiles.length];
-            filepaths[0] = listfiles[0].getAbsolutePath();
-            filenames[0] = listfiles[0].getName();
-            try {
-                FileInputStream fis = new FileInputStream(filepaths[0]);
-                ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                byte[] buf = new byte[1024];
+            for (x=0; x<listfiles.length; x++) {
+                filepaths[x] = listfiles[i].getAbsolutePath();
+                filenames[x] = listfiles[i].getName();
+            }
+
+            for (y=0; y<listfiles.length;y++ ) {
                 try {
-                    for (int readNum; (readNum = fis.read(buf)) != -1; ) {
-                        //Writes to this byte array output stream
-                        bos.write(buf, 0, readNum);
-                        System.out.println("read " + readNum + " bytes,");
+                    FileInputStream fis = new FileInputStream(filepaths[0]);
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    byte[] buf = new byte[1024];
+                    try {
+                        for (int readNum; (readNum = fis.read(buf)) != -1; ) {
+                            //Writes to this byte array output stream
+                            bos.write(buf, 0, readNum);
+                            System.out.println("read " + readNum + " bytes,");
+                        }
+                    } catch (IOException ex) {
+                        Toast.makeText(this, "I/O Exception", Toast.LENGTH_SHORT).show();
                     }
-                } catch (IOException ex) {
-                    Toast.makeText(this, "I/O Exception", Toast.LENGTH_SHORT).show();
-                }
-                byte[] bytes = bos.toByteArray();
-                String encodedString = Base64.encodeToString(bytes, Base64.URL_SAFE | Base64.NO_WRAP);
-                byte[] decodedArray = Base64.decode(encodedString, Base64.URL_SAFE | Base64.NO_WRAP);
-                if (bytes.equals(decodedArray)) {
-                    //Toast.makeText(this, "True", Toast.LENGTH_LONG).show();
-                }
-                for (i=0;i<bytes.length;i++) {
-                    byteArray = byteArray + ", " + bytes[i];
-                    decodearray = decodearray + ", " + decodedArray[i];
+                    byte[] bytes = bos.toByteArray();
+                    String encodedString = Base64.encodeToString(bytes, Base64.URL_SAFE | Base64.NO_WRAP);
+                    byte[] decodedArray = Base64.decode(encodedString, Base64.URL_SAFE | Base64.NO_WRAP);
+                    if (bytes.equals(decodedArray)) {
+                        //Toast.makeText(this, "True", Toast.LENGTH_LONG).show();
+                    }
+                    for (i = 0; i < bytes.length; i++) {
+                        byteArray = byteArray + ", " + bytes[i];
+                        decodearray = decodearray + ", " + decodedArray[i];
 
-                    Log.i("Now","value :" + i);
-                }
+                        Log.i("Now", "value :" + i);
+                    }
 
-                //-----------------------------------------------------------------
-                //just for confirmation
-                for (i=0; i<bytes.length; i++) {
-                    for (j=0;j<bytes.length; j++) {
-                        if(bytes[i] == decodedArray[j]) {
-                            if(i==j) {
-                                count++;
+                    //-----------------------------------------------------------------
+                    //just for confirmation
+                    for (i = 0; i < bytes.length; i++) {
+                        for (j = 0; j < bytes.length; j++) {
+                            if (bytes[i] == decodedArray[j]) {
+                                if (i == j) {
+                                    count++;
+                                }
                             }
                         }
                     }
+                    //-----------------------------------------------------------------
+                    if (count == bytes.length) {
+                        Toast.makeText(this, "Both are equal :)", Toast.LENGTH_SHORT).show();
+                    }
+                    //Toast.makeText(this,byteArray.length() + "||" + decodearray.length(),Toast.LENGTH_SHORT).show();
+                    if (byteArray.equals(decodearray)) {
+                        //Toast.makeText(this, "True", Toast.LENGTH_SHORT).show();
+                    }
+                    HashMap fileList = new HashMap();
+                    fileList.put("filename",filenames[i]);
+                    fileList.put("image_byteArray", encodedString);
+                    
+                } catch (FileNotFoundException ex) {
+                    Toast.makeText(this, "File Not Found", Toast.LENGTH_LONG).show();
                 }
-                //-----------------------------------------------------------------
-                if(count == bytes.length) {
-                    Toast.makeText(this, "Both are equal :)", Toast.LENGTH_SHORT).show();
-                }
-                //Toast.makeText(this,byteArray.length() + "||" + decodearray.length(),Toast.LENGTH_SHORT).show();
-                if(byteArray.equals(decodearray)) {
-                    //Toast.makeText(this, "True", Toast.LENGTH_SHORT).show();
-                }
-
-            } catch (FileNotFoundException ex) {
-                Toast.makeText(this, "File Not Found", Toast.LENGTH_LONG).show();
             }
 /*            if (bmp3.equals(bmp4)) {
                 Toast.makeText(this, "2nd True", Toast.LENGTH_SHORT).show();
